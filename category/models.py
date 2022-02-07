fcfa3b84b36b65afbd856999a7a9a8d78174a575
Uttam <uk2242004@gmail.com>
2022-02-07 11:16:23 +0530

    E-commerec portal using django

---
+++ category/models.py
@@ -0,0 +1,22 @@
+from distutils.command.upload import upload
+from tabnanny import verbose
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+class category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=255, unique=True)
+    cat_image = models.ImageField(upload_to='photos/categories', blank=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])
+
+    def __str__(self):
+        return self.category_name
