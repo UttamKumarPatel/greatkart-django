e8644053031d08a7940ddf6153b426edddea99bb
Uttam <uk2242004@gmail.com>
2022-03-15 12:53:55 +0530

    Django website authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -135,3 +135,16 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ukpatel029@gmail.com'
+EMAIL_HOST_PASSWORD = 'Uttam1112000'
+EMAIL_USE_TLS = True
